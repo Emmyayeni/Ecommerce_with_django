337003222811ab87a8751edb24ff94cc6358e42b
Ayeni Emmanuel <90089286+Emmyayeni@users.noreply.github.com>
2023-11-11 17:26:37 +0100

    Update settings.py

--- Fcommerce/settings.py
+++ Fcommerce/settings.py
@@ -20,9 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = SECRET_KEY = 'django-insecure-@^1y(ftjk=tf-w%95b7$hu=ov$35kf)c&dcuarv*7lwzegw9bv'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
+DEBUG = False
 if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
